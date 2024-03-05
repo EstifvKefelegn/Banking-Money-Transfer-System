9324b1935063166ada8ebe15775fd609f48ec44f
Estifanos <Kefelegnestifanos0@gmail.com>
2024-03-05 09:42:05 +0300

    Adding Envvariable

--- paymentproj/settings.py
+++ paymentproj/settings.py
@@ -11,8 +11,11 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
 import os
 
+# generating and printing the SECRET_KEY
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
