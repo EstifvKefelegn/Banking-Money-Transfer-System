486f41bcfe3d64aa371264a00ca8701a1ab711de
Estifanos <Kefelegnestifanos0@gmail.com>
2024-03-05 11:03:46 +0300

    New customisation

--- paymentproj/settings.py
+++ paymentproj/settings.py
@@ -101,14 +101,21 @@ WSGI_APPLICATION = 'paymentproj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'banking',
+#         'USER': 'postgres',
+#         'PASSWORD': 'Birth@19911215',
+#         'HOST': 'localhost',
+#         'PORT': '5432'
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'banking',
-        'USER': 'postgres',
-        'PASSWORD': 'Birth@19911215',
-        'HOST': 'localhost',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
